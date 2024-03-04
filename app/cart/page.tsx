9e27c01905f54b9b9b443fe8c03d1595261085eb
unknown <admin@L203-20.ADC.HUMANITIES.MN>
2024-03-04 18:49:27 +0800

    s

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,45 +1,15 @@
 "use client";
-
-import { GlobalContext } from "@/context/GlobalContext";
-import Image from "next/image";
-import { Button } from "primereact/button";
-import { useContext } from "react";
-
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from "../components/PaymentForm";
 const CartPage = () => {
-  const { cart }: any = useContext(GlobalContext);
-
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  );
   return (
-    <div>
-      <h1 className="m-10 font-bold text-lg">
-        Таны сагсанд {cart.length} сургалт байна.
-      </h1>
-      <div className="flex flex-col gap-5">
-        {cart.map((course: any, index: number) => (
-          <div key={index} className="flex items-center gap-2">
-            <Image
-              alt="image"
-              width={100}
-              height={100}
-              src="/video.jpg"
-              className="object-cover"
-            />
-
-            <div>
-              <h1>{course.title}</h1>
-
-              <span>{course.price}</span>
-            </div>
-
-            <Button label="Устгах" />
-          </div>
-        ))}
-      </div>
-
-      <Button
-        label="Сагс доторх сургалтуудыг худалдаж авах"
-        className="bg-teal-800 p-2 text-white text-sm my-10 hover:bg-teal-400"
-      />
-    </div>
+    <Elements stripe={stripePromise}>
+      <PaymentForm />
+    </Elements>
   );
 };
 
